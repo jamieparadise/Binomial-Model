290c36cf830ccdefaff62654b43242ca3dc8dadb
Jamie Paradise <69188328+jamieparadise@users.noreply.github.com>
2020-08-10 15:32:56 +0100

    Add files via upload

--- 2 Step Binomial Model/main.cpp
+++ 2 Step Binomial Model/main.cpp
@@ -64,10 +64,11 @@ int main()
     std::cout << "So C = "<< CallPrices[0][0] <<std::endl; 
     std::cout << "===================== " <<std::endl;
     std::cout << "Hedging " <<std::endl;
-    std::cout << "===================== " <<std::endl;   
-    double r{0.05}; // annual riskfree rate
-    double R{log(1+r)};
-    double discount_factor {exp(-R*T/2)};
+    std::cout << "===================== " <<std::endl;
+    
+    double R{0.05}; // riskfree return over one year
+    double r{log(1+R)};//0.04879... continuously compounded riskfree rate
+    double discount_factor {exp(-r*T/2)};
     std::cout << "If the interest rate is " << r <<std::endl;
     std::cout << "If the stock goes up to " << StockPrices[1][0] <<  " at time t = "<< T/2 << " years and we want to hedge with a portfolio of S_u * delta_u - C_u in the next step" << std::endl;
     std::cout << "By having a portfolio of " << delta_u << " * S_u - C_u we are hedged again moves in the stock price." << std::endl;
@@ -95,43 +96,43 @@ int main()
     std::cout << "Replication " <<std::endl;
     std::cout << "===================== " <<std::endl;
     double A_0{100};
-    double A_n{A_0*exp(R*T)}; //A_0*(r+1)};
+    double A_n{A_0*exp(r*T)}; //A_0*(r+1)};
     std::cout << "A call option can be replicated by a portfolio of x stock and y bonds/cash." <<std::endl;
-    std::vector<double> BondPrices {StockPrices[0][0],StockPrices[0][0]*exp(R*T/2),StockPrices[0][0]*exp(R*T)};
+    std::vector<double> BondPrices {StockPrices[0][0],StockPrices[0][0]*exp(r*T/2),StockPrices[0][0]*exp(r*T)};
     std::cout << "C = xS + yA" <<std::endl;
     std::cout << "Where A is bonds/cash worth " << BondPrices[0] << " initially, "<< BondPrices[1] << " after " << T/2 << " years and " << BondPrices[2] << " after " << T << " years"<<std::endl;
 
     std::cout << "If the stock goes up in the first step:" << std::endl; 
-    std::cout << "C_u(T) = xS_u(T) + yA(T)"<< " where t="<<T/2<< " or " << T <<std::endl;
+    std::cout << "C_u(t) = xS_u(t) + yA(t)"<< " where t="<<T/2<< " or " << T <<std::endl;
     std::cout << "C_uu = xS_uu + yA("<<T<<")"<<std::endl;
     double y_u{(CallPrices[2][0] - delta_u * StockPrices[2][0])/BondPrices[2]};
     std::cout << CallPrices[2][0]  << " = " << delta_u << " * "  << StockPrices[2][0] << " + y * " << BondPrices[2] <<std::endl;
     std::cout << "y = " << y_u << std::endl;
-    std::cout << "The call C_u(T) is replicated by a portfolio of stocks and bonds/cash between t="<<T/2<< " and " << T << std::endl;
-    std::cout << "C_u(T) = "<<  delta_u << " S_u(T) "  <<  " + "<< y_u << " A(T)"<<std::endl;
+    std::cout << "The call C_u(t) is replicated by a portfolio of stocks and bonds/cash between t="<<T/2<< " and " << T << std::endl;
+    std::cout << "C_u(t) = "<<  delta_u << " S_u(t) "  <<  " + "<< y_u << " A(t)"<<std::endl;
     std::cout << "Therefore: "<<std::endl;
     std::cout << "C_u("<<T/2<<") = "<<  delta_u << " S_u("<<T/2<<") "  <<  " + "<< y_u << " A("<<T/2<<")"<<std::endl;
     std::cout << "C_u("<<T/2<<") = "<<  delta_u << " * " <<  StockPrices[1][0] <<  " + " << y_u << " * "<<  BondPrices[1] << " = "<< delta_u * StockPrices[1][0] + y_u * BondPrices[1] <<std::endl;
     
     std::cout << "\nInstead, if the stock goes down in the first step:" << std::endl; 
-    std::cout << "C_d(T) = xS_d(T) + yA(T)"<< " where t="<<T/2<< " or " << T <<std::endl;
+    std::cout << "C_d(t) = xS_d(t) + yA(t)"<< " where t="<<T/2<< " or " << T <<std::endl;
     std::cout << "C_du = xS_du + yA("<<T<<")"<<std::endl;
     std::cout << CallPrices[2][1]  << " = " << delta_d << " * "  << StockPrices[2][1] << " + y * " << BondPrices[2] <<std::endl;
     double y_d{(CallPrices[2][1] - delta_d * StockPrices[2][1])/BondPrices[2]};
     std::cout << "y = " << y_d << std::endl;
-    std::cout << "The call C_d(T) is replicated by a portfolio of stocks and bonds/cash between t="<<T/2<< " and " << T << std::endl;
-    std::cout << "C_d(T) = "<<  delta_d << " S_d(T) "  <<  " + "<< y_d << " A(T)"<<std::endl;
+    std::cout << "The call C_d(t) is replicated by a portfolio of stocks and bonds/cash between t="<<T/2<< " and " << T << std::endl;
+    std::cout << "C_d(t) = "<<  delta_d << " S_d(t) "  <<  " + "<< y_d << " A(T)"<<std::endl;
     std::cout << "Therefore: "<<std::endl;
     std::cout << "C_d("<<T/2<<") = "<<  delta_d << " S("<<T/2<<") "  <<  " + "<< y_d << " A("<<T/2<<")"<<std::endl;
     std::cout << "C_d("<<T/2<<") = "<<  delta_d << " * " <<  StockPrices[1][0] <<  " + " << y_d << " * "<<  BondPrices[1] << " = "<< delta_d * StockPrices[1][0] + y_d * BondPrices[1] <<std::endl;
     
-    std::cout << "\nFinally, between t=0 and "<< T/2 <<" the call can be replicated by a portfolio of x stock and y bonds/cash, C(T) = xS(T) + yA(T)"<< " where T="<<0<< " or " << T/2 <<std::endl;
+    std::cout << "\nFinally, between t=0 and "<< T/2 <<" the call can be replicated by a portfolio of x stock and y bonds/cash, C(t) = xS(t) + yA(t)"<< " where t="<<0<< " or " << T/2 <<std::endl;
     std::cout << "C_u = xS_u + yA("<<T/2<<")"<<std::endl;
     std::cout << CallPrices[1][0]  << " = " << delta << " * "  << StockPrices[1][0] << " + y * " << BondPrices[1] <<std::endl;
     double y{(CallPrices[1][0] - delta * StockPrices[1][0])/(BondPrices[1])};
     std::cout << "y = " << y << std::endl;    
-    std::cout << "The call C(T) is replicated by a portfolio of stocks and bonds/cash between t="<<0<< " and " << T/2 << std::endl;
-    std::cout << "C(T) = "<<  delta << " S(T) "  <<  " + "<< y << " A(T)"<<std::endl;
+    std::cout << "The call C(t) is replicated by a portfolio of stocks and bonds/cash between t="<<0<< " and " << T/2 << std::endl;
+    std::cout << "C(t) = "<<  delta << " S(t) "  <<  " + "<< y << " A(t)"<<std::endl;
     std::cout << "Therefore: "<<std::endl;
     std::cout << "C("<<0<<") = "<<  delta << " S("<<0<<") "  <<  " + "<< y << " A("<<0<<")"<<std::endl;
     std::cout << "C("<<0<<") = "<<  delta << " * " <<  StockPrices[0][0] <<  " + " << y << " * "<<  BondPrices[0] << " = "<< delta * StockPrices[0][0] + y * BondPrices[0] <<std::endl;
@@ -141,24 +142,24 @@ int main()
     std::cout << "The value of the call can be calculated with risk neutral pricing." <<std::endl;
     std::cout <<  "The value of the call is the discounted expected value of the future payoff under the risk-neutral random walk" <<std::endl;
     std::cout << "If the stock goes up in the first step:" << std::endl; 
-    std::cout << "The value of the call at t="<<T/2<< ", C_u("<<T/2<<")= (p * C_uu("<<T<<") + (1 - p) * C_ud("<<T<<"))*exp(-(R*"<<T/2<<"))" <<std::endl;
+    std::cout << "The value of the call at t="<<T/2<< ", C_u("<<T/2<<")= (p * C_uu("<<T<<") + (1 - p) * C_ud("<<T<<"))*exp(-(r*"<<T/2<<"))" <<std::endl;
     std::cout << "Where p is the risk neutral probably of the stock going up." <<std::endl;
-    double p_u{(StockPrices[1][0]*exp(R*T*0.5)-StockPrices[2][1])/(StockPrices[2][0]-StockPrices[2][1]) };//((1+r)*S_0-S_d)/(S_u-S_d)};
+    double p_u{(StockPrices[1][0]*exp(r*T*0.5)-StockPrices[2][1])/(StockPrices[2][0]-StockPrices[2][1]) };//((1+r)*S_0-S_d)/(S_u-S_d)};
     std::cout << "p = "<< p_u << std::endl;
-    std::cout << "The value of C_u(T/2) = ("<< p_u <<  " * C_uu(T) + "<<  (1 - p_u) << "* C_d(T))*exp(-(R*T/2))." <<std::endl;
+    std::cout << "The value of C_u(T/2) = ("<< p_u <<  " * C_uu(T) + "<<  (1 - p_u) << "* C_d(T))*exp(-(r*T/2))." <<std::endl;
     std::cout << "The value of C_u("<<T/2<< ") = "<< (p_u * CallPrices[2][0] +  (1 - p_u)* CallPrices[2][1] )*discount_factor <<std::endl;
     std::cout << "\nInstead, if the stock goes down in the first step:" << std::endl; 
-    std::cout << "The value of the call at t="<<T/2<< ", C_d("<<T/2<<")= (p * C_du("<<T<<") + (1 - p) * C_dd("<<T<<"))*exp(-(R*"<<T/2<<"))" <<std::endl;
+    std::cout << "The value of the call at t="<<T/2<< ", C_d("<<T/2<<")= (p * C_du("<<T<<") + (1 - p) * C_dd("<<T<<"))*exp(-(r*"<<T/2<<"))" <<std::endl;
     std::cout << "Where p is the risk neutral probably of the stock going up." <<std::endl;
-    double p_d{(StockPrices[1][1]*exp(R*T*0.5)-StockPrices[2][2])/(StockPrices[2][1]-StockPrices[2][2]) };
+    double p_d{(StockPrices[1][1]*exp(r*T*0.5)-StockPrices[2][2])/(StockPrices[2][1]-StockPrices[2][2]) };
     std::cout << "p = "<< p_d << std::endl;
-    std::cout << "The value of C_d(T/2) = ("<< p_d <<  " * C_du(T) + "<<  (1 - p_d) << "* C_dd(T))*exp(-(R*T/2))." <<std::endl;
+    std::cout << "The value of C_d(T/2) = ("<< p_d <<  " * C_du(T) + "<<  (1 - p_d) << "* C_dd(T))*exp(-(r*T/2))." <<std::endl;
     std::cout << "The value of C_d("<<T/2<< ") = "<< (p_d * CallPrices[2][1] +  (1 - p_d)* CallPrices[2][2] )*discount_factor <<std::endl; 
-    std::cout << "\nFinally, the value of the call at t="<<0<< ", C"<<0<<")= (p * C_u("<<T/2<<") + (1 - p) * C_d("<<T/2<<"))*exp(-(R*"<<T/2<<"))" <<std::endl;
+    std::cout << "\nFinally, the value of the call at t="<<0<< ", C"<<0<<")= (p * C_u("<<T/2<<") + (1 - p) * C_d("<<T/2<<"))*exp(-(r*"<<T/2<<"))" <<std::endl;
     std::cout << "Where p is the risk neutral probably of the stock going up." <<std::endl;
-    double p{(StockPrices[0][0]*exp(R*T*0.5)-StockPrices[1][1])/(StockPrices[1][0]-StockPrices[1][1]) };
+    double p{(StockPrices[0][0]*exp(r*T*0.5)-StockPrices[1][1])/(StockPrices[1][0]-StockPrices[1][1]) };
     std::cout << "p = "<< p << std::endl;
-    std::cout << "The value of C(0) = ("<< p <<  " * C_u(T/2) + "<<  (1 - p) << "* C_d(T/2))*exp(-(R*T/2))." <<std::endl;
+    std::cout << "The value of C(0) = ("<< p <<  " * C_u(T/2) + "<<  (1 - p) << "* C_d(T/2))*exp(-(r*T/2))." <<std::endl;
     std::cout << "The value of C("<<0<< ") = "<< (p * CallPrices[1][0] +  (1 - p)* CallPrices[1][1] )*discount_factor <<std::endl;
     return 0;
 }
