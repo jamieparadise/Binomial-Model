77320860f9ebf4c7f6d9c98f807c33e592c053e4
Jamie Paradise <69188328+jamieparadise@users.noreply.github.com>
2020-08-11 21:04:18 +0100

    Add files via upload
    I created a call for the class with functions to create the stock and call price trees, print those trees out or recalculate the call with a different number of steps

--- Bimonial Tree CRR/main.cpp
+++ Bimonial Tree CRR/main.cpp
@@ -1,70 +1,7 @@
+#include "BinomTreeCall.h"
 #include <iostream>
 #include <vector>
 #include <cmath>
-
-double binom_tree_call_price(int N, double S, double X, double r,double T, double sigma) {
-    double time_step{T/N}; //size of each time step
-    // CRR method
-    double u{exp(sigma*sqrt(time_step))}; // the amount the stock goes up 
-    double d{1/u};                      // or down at each step
-    double p{(exp(r*time_step)-d)/(u-d)}; // the risk neutral probability of the stock moving up at each step
-
-    std::vector<std::vector<double>> StockPriceTree(N+1,std::vector<double>(N+1));
-    
-    for(int i=0;i<=N;i++){
-        for(int j=0;j<=i;j++){
-            StockPriceTree[i][j]=S*pow(u,j)*pow(d,i-j);
-            }
-        std::cout << std::endl;
-    }
-    
-    for(int i=0;i<=N;i++){
-        std::cout << "The stock prices at step " << i <<": " << std::endl;
-        for(int j=i;j>=0;j--){
-            std::cout  << StockPriceTree[i][j] << std::endl;
-        }
-        std::cout << std::endl;
-    }
-	
-    //next the tree of the call prices is created
-    std::vector<std::vector<double>> CallPriceTree(N+1,std::vector<double>(N+1));
-    for(int i=0;i<=N;i++)
-    {
-        //the prices of the calls are calculated at expiry using the asset prices at the end final time step
-        CallPriceTree[N][i]=std::max(StockPriceTree[N][i]-X,0.0);
-    }
-// These call prices are used to calculate the prices of the calls back down the tree.                                                                                                                
-// Using risk neutral pricing, the price of the call is the discounted expected value of the future payoff.            
-// So the price of the call at time t can be found by looking at the value of the two calls at t+1,                   
-// that are above and below it in the tree, and multiplying the those values by their risk neutral probability,       
-// then summing them and multiplying by the discount factor C(T) = (p * C_u(T+1) + (1-p) * C_d(T+1)) * e ^ (-r*t)        
-//                                                                                                                      
-//     p     C_u(T+1)
-//          /
-//      C(T)
-//          \
-//     1-p   C_d(T+1)
-//                                                                                                            
-     
-    for(int i=N-1;i>=0;i--)
-    {
-        for(int j=0;j<=i;j++)
-        {
-            CallPriceTree[i][j] = exp(-r*time_step)*( p*CallPriceTree[i+1][j+1] + (1-p)*CallPriceTree[i+1][j]);
-        }
-    }
-    
-    for(int i=N;i>=0;i--)
-    {
-        std::cout << "The call prices at step " << i <<": " << std::endl;
-        for(int j=i;j>=0;j--)
-        {
-            std::cout << CallPriceTree[i][j] << std::endl;
-        }
-        std::cout << std::endl;
-    }
-    return CallPriceTree[0][0];
-}
 
 int main(int argc, char **argv)
 {
@@ -72,11 +9,17 @@ int main(int argc, char **argv)
     double X  {100};  // strike price
     double R{0.05}; //riskfree return over one year
     double r{log(1+R)};//0.04879... continuously compounded riskfree rate
-    double T {2};    // time until expiry
-    double sigma {0.1};  //volatility
-    int N{2}; // number of time steps
-    double call_price {binom_tree_call_price(N,S,X,r,T,sigma)};
-    std::cout << "The call price with N="<<N<< ", S="<<S<<", X="<<X<<", r="<<r<<", T="<<T<<", sigma="<<sigma<<": "<< call_price << std::endl;
-
+    double T {1};    // time until expiry
+    double sigma {0.085};  //volatility
+    int N{4}; // number of time steps
+   // double call_price {binom_tree_call_price(N,S,X,r,T,sigma)};
+    BinomTreeCall call_tree(N,S,X,R,T,sigma); 
+    double price{};
+    std::cout << "The call price with N="<<N<< ", S="<<S<<", X="<<X<<", r="<<r<<", T="<<T<<", sigma="<<sigma<<": "<< call_tree.return_call_price()<< std::endl;
+    for(int i=500;i<=3000;i+=500){
+        price = call_tree.calc_call_price(i);
+        std::cout << "The call price with N="<<i<< ", S="<<S<<", X="<<X<<", r="<<r<<", T="<<T<<", sigma="<<sigma<<": " << price<<std::endl; 
+    }
+    std::cout << "The call seems to get about 3 decimal place accuracy with 2000-3000 steps"<< std::endl;
 	return 0;
 }
